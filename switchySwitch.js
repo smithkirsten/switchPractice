72962dfeae0ebd62ea4004964d0e1f912f21d947
Kirsten Smith <brownsmith.kirsten@gmail.com>
2022-09-15 11:08:56 -0400

    add switch case name

---
+++ switchySwitch.js
@@ -0,0 +1,25 @@
+var name = "Alden";
+
+switch(name) {
+  case "Kirsten":
+    console.log("Wow, what a cool name!");
+    break;
+  case "Kristen":
+    console.log("Yikes, sorry...");
+    break;
+  default:
+  console.log(`Nice to meet you, ${name}`);
+}
+
+// You are given a variable, . Your task is to print:
+//
+// - ONE, if it is equal to 1.
+// - TWO, if it is equal to 2.
+// - THREE, if it is equal to 3.
+// - FOUR, if it is equal to 4.
+// - FIVE, if it is equal to 5.
+// - SIX, if it is equal to 6.
+// - SEVEN, if it is equal to 7.
+// - EIGHT, if it is equal to 8.
+// - NINE, if it is equal to 9.
+// - PLEASE TRY AGAIN, if it is none of the above.
