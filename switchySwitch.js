343e31b743c6a0a2df413c063e066a2c6380a321
Kirsten Smith <brownsmith.kirsten@gmail.com>
2022-09-15 11:18:56 -0400

    Add switch case numbers

--- switchySwitch.js
+++ switchySwitch.js
@@ -23,3 +23,38 @@ switch(name) {
 // - EIGHT, if it is equal to 8.
 // - NINE, if it is equal to 9.
 // - PLEASE TRY AGAIN, if it is none of the above.
+
+ var num;
+
+ switch(num) {
+  case 1:
+    console.log("ONE");
+    break;
+  case 2:
+    console.log("TWO");
+    break;
+  case 3:
+    console.log("THREE");
+    break;
+  case 4:
+    console.log("FOUR");
+    break;
+  case 5:
+    console.log("FIVE");
+    break;
+  case 6:
+    console.log("SIX");
+    break;
+  case 7:
+   console.log("SEVEN");
+   break;
+  case 8:
+    console.log("EIGHT");
+    break;
+  case 9:
+    console.log("NINE");
+    break;
+  default:
+     console.log("Please try again");
+     break;
+ }
